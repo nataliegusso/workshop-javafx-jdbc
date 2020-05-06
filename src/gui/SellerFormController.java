ffafb979da1aa679937e9684e256e696818df947
nataliegusso <nataliegusso@gmail.com>
2020-05-06 14:55:16 -0300

    Project completed

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -34,7 +36,6 @@ import model.entities.Seller;
 import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 import model.services.SellerService;
-
 
 public class SellerFormController implements Initializable {   //configurações do controller
 	
@@ -131,15 +132,34 @@ public class SellerFormController implements Initializable {   //configurações d
 
 		ValidationException exception = new ValidationException("Validation error");
 		
-		obj.setId(Utils.tryParseToInt(txtId.getText())); //chama a função que pega a string da caixa e converte num inteiro
+		obj.setId(Utils.tryParseToInt(txtId.getText())); //chama a função que pega a string da caixa e converte num inteiro, se der errado ele passa um nulo
 		
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {  //campo nome não pode ser vazio //trim: elimina espaços em branco no início ou fim
 			exception.addError("name", "Field can't be empty");  //chama o método na classe ValidationException
 		}
-		
 		obj.setName(txtName.getText());  //pega o nome digitado
+				
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		}
+		else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));  //atStartOfDay: converte a data escolhida no horário do computador do usuário para o instant q é uma data independentemente de localidade
+			obj.setBirthDate(Date.from(instant));  //o meu obj espera um dado do tipo Date, tem que converter o instant
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));  //é string e tem que converter em double, mas pode dar erro ao preencher, daí ele retorna um nulo  //está lá no utils (F3)
+
+		obj.setDepartment(comboBoxDepartment.getValue());
 		
-		if (exception.getErrors().size() > 0) {  //se na minha coleção de erros tivr pelo menos um erro, lança exceção
+		if (exception.getErrors().size() > 0) {   //se na minha coleção de erros tivr pelo menos um erro, lança exceção
 			throw exception;
 		}
 		
@@ -156,6 +176,15 @@ public class SellerFormController implements Initializable {   //configurações d
 		initializeNodes();
 	}
 	
+	private void initializeNodes() {  //Restrições
+		Constraints.setTextFieldInteger(txtId);  //Só aceitar inteiro
+		Constraints.setTextFieldMaxLength(txtName, 70);  //Tamanho máximo
+		Constraints.setTextFieldDouble(txtBaseSalary);  //Double
+		Constraints.setTextFieldMaxLength(txtEmail, 60);  //Tamanho máximo
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");  //formato p data
+		initializeComboBoxDepartment();  //ComboBox com lista de departments
+	}
+	
 	public void updateFormData() {  //pega os dados do seller e coloca nas caixas do formulários
 		if (entity == null) {		//programação defensiva caso o programador esqueça de colocar os valores
 			throw new IllegalStateException("Entity was null");
@@ -169,7 +198,6 @@ public class SellerFormController implements Initializable {   //configurações d
 		if (entity.getBirthDate() != null) {
 //			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 			dpBirthDate.setValue(LocalDateTime.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()).toLocalDate());
-//			dpBirthDate.setValue(LocalDateTime.ofInstant(this.seller.getBirthDate().toInstant(), ZoneId.systemDefault()).toLocalDate());
 		}
 		
 		if (entity.getDepartment() == null) {
@@ -188,21 +216,20 @@ public class SellerFormController implements Initializable {   //configurações d
 		comboBoxDepartment.setItems(obsList);  //associa a lista ao combobox
 	}
 	
-	private void initializeNodes() {  //Restrições
-		Constraints.setTextFieldInteger(txtId);  //Só aceitar inteiro
-		Constraints.setTextFieldMaxLength(txtName, 70);  //Tamanho máximo
-		Constraints.setTextFieldDouble(txtBaseSalary);  //Double
-		Constraints.setTextFieldMaxLength(txtEmail, 60);  //Tamanho máximo
-		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");  //formato p data
-		initializeComboBoxDepartment();  //ComboBox com lista de departments
-	}
-
 	private void setErrorMessages(Map<String, String> errors) {	//percorre a lista de erros da exceção e escreve no formulário
 		Set<String> fields = errors.keySet();  
 
-		if (fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		//if (fields.contains("name")) {
+		//	labelErrorName.setText(errors.get("name"));  //joga a mensagem que está no name
+		//}else {
+		//	labelErrorName.setText("");  //não joga a mensagem ou apaga a mensagem
+		//}
+
+		//operador condicional ternário: labelErrorName.setText((fields.contains("name") ? True : False)
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));  
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
 	}
 	
 	private void initializeComboBoxDepartment() {  //chama lá no initializeNodes
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -29,6 +29,14 @@ public class Utils {
 		}
 	}
 
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
 	//https://stackoverflow.com/questions/47484280/format-of-date-in-the-javafx-tableview
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) { // formatar data  //profe não explicou Crtl+C Ctrl+V
 		tableColumn.setCellFactory(column -> {
